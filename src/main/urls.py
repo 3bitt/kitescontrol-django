f9398ef629e410d8069b923801681029a9975bc6
3bitt <batoniczny@gmail.com>
2020-10-17 17:30:31 +0200

    old files deletion

--- src/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import Index
-
-app_name = 'main'
-urlpatterns = [
-    path('', Index.as_view()),
-]
--- src/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import View, TemplateView
-# Create your views here.
-
-
-class Index(TemplateView):
-    template_name = 'main/base.html'
