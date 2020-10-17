fdef68311b974c8898e7f0471f0206a14ab7ba37
3bitt <batoniczny@gmail.com>
2020-10-17 20:14:21 +0200

    dashboard app added

--- kitescontrol/settings.py
+++ kitescontrol/settings.py
@@ -1,3 +1,4 @@
+import os, sys
 """
 Django settings for kitescontrol project.
 
@@ -10,7 +11,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+# from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'dashboard',
+    'instructor',
+    'student',
+    'lesson'
 ]
 
 MIDDLEWARE = [
