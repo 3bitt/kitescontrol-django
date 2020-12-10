650efc913f38a6757e5f61c0bb21e29a2bd659ee
3bitt <batoniczny@gmail.com>
2020-12-10 03:47:01 +0100

    s

--- apps/instructor/instructor/views.py
+++ apps/instructor/instructor/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, CreateView,DetailView,UpdateView,DeleteView
 from .models import Instructor
