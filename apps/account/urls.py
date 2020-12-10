4b575b55f9c5b57c6f15fe64db479f0f235562ea
3bitt <batoniczny@gmail.com>
2020-12-10 02:47:37 +0100

    clear git cache

--- apps/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import (MainLoginView)
-
-app_name = 'account'
-urlpatterns = [
-    path('', MainLoginView.as_view(), name='login'),
-]
