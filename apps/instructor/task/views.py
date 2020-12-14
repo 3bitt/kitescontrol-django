bb0da07f7e6bbf76e02a3f69f0a90ba4490e30ec
3bitt <batoniczny@gmail.com>
2020-12-14 22:25:55 +0100

    task list/create/edit+update corrections and new features

--- apps/instructor/task/views.py
+++ apps/instructor/task/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.shortcuts import redirect
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 from .models import Task
 from .forms import TaskCreateForm
 from django.urls import reverse_lazy
@@ -16,11 +16,39 @@ class TaskListView(ListView):
         context["tasks_dates"] = self.queryset.values_list('deadline_date',flat=True).distinct()
         return context
 
-
-
 class TaskCreateView(CreateView):
     model = Task
     template_name = 'task/task_create.html'
     form_class = TaskCreateForm
     success_url = reverse_lazy('task:task-list')
 
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'task/task_edit.html'
+    form_class = TaskCreateForm
+    update_status = False
+
+    def get_success_url(self):
+        return reverse_lazy('task:task-list')
+
+    def form_valid(self, form):
+        if form.instance.completed_flag == True:
+            self.object.completed_date = date.today()
+            return super(TaskUpdateView, self).form_valid(form)
+        else:
+            return super(TaskUpdateView, self).form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        if self.update_status == True:
+            self.object = self.get_object()
+            self.object.completed_date = date.today()
+            self.object.completed_flag = True
+            self.object.save()
+            return redirect('task:task-list')
+        else:
+            return super().post(self, request, *args, **kwargs)
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    success_url = reverse_lazy('task:task-list')
