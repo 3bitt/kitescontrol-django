1265e31ab8b08a5294d963f9916db2262c9cd946
3bitt <batoniczny@gmail.com>
2020-12-22 00:22:34 +0100

    fun with code

--- apps/lesson/views.py
+++ apps/lesson/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView, CreateView, DetailView, DeleteView
 from django.db.models import Q
 from instructor.instructor.models import Instructor
 from .models import Lesson
-from datetime import date
+from datetime import date, datetime
 from .forms import LessonCreateForm
 from django.urls import reverse_lazy
 
@@ -16,16 +16,17 @@ class LessonListView(ListView):
     queryset = Instructor.objects.all()
     template_name = 'lesson/lesson_list.html'
     context_object_name = 'instructors_all'
-    print('CURR DATE: ', current_date)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         today_lessons = Lesson.objects.filter(start_date=self.current_date).order_by('start_time')
-
         context['instructors_with_lessons'] = self.queryset.prefetch_related(
             Prefetch('lessons', today_lessons)
         )
         context['hours'] = range(7,22)
+        context['current_date'] = self.current_date
+        context['current_time'] = datetime.today().hour
+        print(context['current_time'])
 
         return context
 
