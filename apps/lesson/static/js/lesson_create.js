47aabaccb4b19ec807874a2843fd9e43a11e7c97
3bitt <batoniczny@gmail.com>
2021-05-08 13:53:03 +0200

    lesson create view changes + schedule redirects

--- apps/lesson/static/js/lesson_create.js
+++ apps/lesson/static/js/lesson_create.js
@@ -2,6 +2,9 @@ const instructors_source_list = Array.from(document.getElementById('id_instructo
 const instructor_picked_list = document.querySelector('.instructor-picked-list');
 const instructor_is_empty_div = document.querySelector('.instructor-not-picked');
 
+const students_source_list = Array.from(document.getElementById('id_student').children);
+const student_picked_list = document.querySelector('.student-picked-list');
+const student_is_empty_div = document.querySelector('.student-not-picked');
 
 instructors_source_list.forEach((instructor) => {
   if (instructor.nodeType === 1) {
@@ -16,7 +19,22 @@ instructors_source_list.forEach((instructor) => {
   }
 })
 
+students_source_list.forEach((student) => {
+  if (student.nodeType === 1) {
+    student.addEventListener('change', (e) => {
+      if (e.target.checked) {
+        pickStudent(student);
+      }
+      else if (!e.target.checked) {
+        removePickedStudent(student);
+      }
+    });
+  }
+})
 
+
+
+// Instructor functions
 function pickInstructor(instr){
   new_instr_div = createInstructorElement(instr);
   instructor_is_empty_div.setAttribute('hidden', 'true');
@@ -49,6 +67,49 @@ function createInstructorElement(instr){
 
   icon.addEventListener('click', (e) => {
     removePickedInstructor(instr);
+  })
+
+  new_div.appendChild(text);
+  new_div.appendChild(icon);
+  return new_div
+}
+
+// -----------------
+// Student functions
+// -----------------
+
+function pickStudent(student) {
+  new_student_div = createStudentElement(student);
+  student_is_empty_div.setAttribute('hidden', 'true');
+  student_picked_list.appendChild(new_student_div);
+}
+
+function removePickedStudent(student) {
+  const target_student_div = document.querySelector(`.${student.firstChild.htmlFor}`);
+  target_student_div.remove();
+
+  const student_checkbox = document.getElementById(`${student.firstChild.htmlFor}`);
+  student_checkbox.checked = false;
+  checkStudentListIfEmpty();
+
+}
+
+function checkStudentListIfEmpty() {
+  if (student_picked_list.children.length == 1) {
+    student_is_empty_div.removeAttribute('hidden')
+  }
+}
+
+function createStudentElement(student) {
+  const new_div = document.createElement('div');
+  new_div.classList.add('student-picked', `${student.firstChild.htmlFor}`);
+  text = document.createTextNode(student.innerText);
+
+  const icon = document.createElement('i')
+  icon.classList.add('fas', 'fa-times', 'remove-icon')
+
+  icon.addEventListener('click', (e) => {
+    removePickedStudent(student);
   })
 
   new_div.appendChild(text);
--- apps/lesson/static/js/lesson_filter_students.js
+++ apps/lesson/static/js/lesson_filter_students.js
@@ -1,22 +1,21 @@
 const parentListElement = Array.from(document.getElementById('id_student').children)
 const searchInput = document.getElementById('student_search_input');
-const studentTakenList = document.getElementById('search_result_list');
 
+// Actual search engine - fired every time user inputs character in search field
+searchInput.addEventListener('input', (event) => {
+  var value = event.target.value;
 
-parentListElement.forEach((item) => {
-  if (item.nodeType === 1) {
-    item.addEventListener('change', (event) =>{
-      if (event.target.checked) {
-        addStudent(item)
+  if (value && value.trim().length > 0) {
+    value = value.trim().toLowerCase();
+    setList(parentListElement.filter(person => {
+      if (!person.innerText.trim().toLowerCase().includes(value)) {
+        person.setAttribute('hidden', true)
       } else {
-
-        for (let node of studentTakenList.children) {
-          if (node.innerText.trim() === item.innerText.trim()) {
-            studentTakenList.removeChild(node)
-          }
-        }
+        person.removeAttribute('hidden')
       }
-    })
+    }));
+  } else {
+    clearOrigList()
   }
 })
 
@@ -34,22 +33,6 @@ function clearOrigList(){
   parentListElement.forEach((item) => item.removeAttribute('hidden'))
 }
 
-function addStudent(obj){
-  const liItem = document.createElement('li');
-  liItem.classList.add('student-picked')
-  const text = document.createTextNode(obj.innerText);
-
-  liItem.appendChild(text)
-  studentTakenList.appendChild(liItem)
-}
-
-function returnOrigList(){
-  clearOrigList()
-  for (const listItem of initialStudentsList) {
-    parentListElement.appendChild(listItem)
-  }
-}
-
 // Set HTML list item to show No Results in case of zero matches
 function setNoResults(){
   const item = document.createElement('li');
@@ -58,20 +41,4 @@ function setNoResults(){
   parentListElement.push(item)
 }
 
-// Actual search engine - fired every time user inputs character in search field
-searchInput.addEventListener('input', (event) => {
-  var value = event.target.value;
 
-  if (value && value.trim().length > 0){
-    value = value.trim().toLowerCase();
-    setList(parentListElement.filter(person => {
-      if (!person.innerText.trim().toLowerCase().includes(value)){
-        person.setAttribute('hidden', true)
-      } else {
-        person.removeAttribute('hidden')
-      }
-    }));
-  } else {
-    clearOrigList()
-  }
-})
