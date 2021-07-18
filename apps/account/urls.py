3bc20004e667ecae7481f60e127c84fe6b8f4ea5
3bitt <batoniczny@gmail.com>
2021-07-18 17:55:34 +0200

    user edit

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import path, reverse, reverse_lazy
-from .views import (AccountCreateUserView, AccountDetailUserView, MainLoginView)
+from .views import (AccountChangeActiveStatus, AccountCreateUserView, AccountDeleteUserView, AccountDetailUserView, AccountUpdateUserView, MainLoginView)
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
 
@@ -13,6 +13,11 @@ urlpatterns = [
 
     path('create-user/', login_required(AccountCreateUserView.as_view()), name='create-user'),
     path('user/<int:pk>/', login_required(AccountDetailUserView.as_view()), name='detail-user'),
+    path('user/update/<int:pk>/', login_required(AccountUpdateUserView.as_view()),
+         name='update-user'),
+
+    path('user/delete/<int:pk>/', login_required(AccountDeleteUserView.as_view()), name='delete-user'),
+    path('user/deactivate/<int:pk>/', login_required(AccountChangeActiveStatus.as_view()), name='deactivate-user'),
 
 
     path('reset_password/',
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.views import LoginView, redirect_to_login
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.urls.base import reverse_lazy
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.urls.base import reverse, reverse_lazy
+from django.views.generic.base import RedirectView, View
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
-
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import TemplateView
 from account.forms import RegisterForm
 from account.models import User
 
@@ -11,6 +13,7 @@ from account.models import User
 class MainLoginView(LoginView):
     template_name = 'account/login.html'
     success_url = '/'
+
 
 class AccountCreateUserView(CreateView):
     template_name = 'account/account_create_user.html'
@@ -27,8 +30,34 @@ class AccountDetailUserView(DetailView):
     queryset = User.objects.all()
     context_object_name = 'user'
 
-class AccountEditUserView(UpdateView):
+class AccountUpdateUserView(UpdateView):
     template_name = 'account/account_update.html'
     model = User
+    fields = ['name', 'surname', 'email']
+    context_object_name = 'user'
+
+    def get_success_url(self) -> str:
+        return reverse('account:detail-user', args=[self.object.id])
+
+
+class AccountDeleteUserView(DeleteView):
+    model = User
+    success_url = reverse_lazy('crew_adm:crew-list')
+
+
+class AccountChangeActiveStatus(RedirectView):
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        user_id = self.kwargs['pk']
+        user = User.objects.get(id=user_id)
+        if user.is_active:
+            user.is_active = False
+        else:
+            user.is_active = True
+        user.save()
+        return HttpResponseRedirect(reverse('account:detail-user', args=[user_id]))
+
+
 
 
