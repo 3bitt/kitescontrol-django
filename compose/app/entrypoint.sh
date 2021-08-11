6e375d8c50ce5a7b3428718c94395fe9c63580fa
3bitt <batoniczny@gmail.com>
2021-08-12 00:52:05 +0200

    entrypoint args extend + wait-for pgsql

--- compose/app/entrypoint.sh
+++ compose/app/entrypoint.sh
@@ -3,6 +3,6 @@
 set -o errexit
 set -o nounset
 
-bash /entrypoint/wait-for-it.sh db-mysql:3306 -- echo "postgres is up"
+bash /entrypoint/wait-for-it.sh db:5432 -- echo "postgres is up"
 
 exec "$@"
